a920f08cb768b0b203e630f3f585e251ae0d36b3
yutadd <spyk4m49@gmail.com>
2022-11-01 20:26:12 +0900

    first commit

---
+++ src/main/java/com/yutadd/AmazingWorldOfEmojiApplication.java
@@ -0,0 +1,13 @@
+package com.yutadd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazingWorldOfEmojiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazingWorldOfEmojiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yutadd/AmazingWorldOfEmojiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yutadd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazingWorldOfEmojiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
