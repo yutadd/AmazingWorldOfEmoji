fdd9e292968128255c63564c5ecf773db5ae8ddb
yutadd <spyk4m49@gmail.com>
2022-11-03 20:49:40 +0900

    make saving password safer

--- src/main/java/com/yutadd/controller/UserController.java
+++ src/main/java/com/yutadd/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.yutadd.controller;
 
-import java.math.BigInteger;
 import java.time.LocalDate;
-import java.util.Random;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,33 +18,42 @@ import com.yutadd.model.SessionID;
 import com.yutadd.model.User;
 import com.yutadd.repository.SessionIDRepository;
 import com.yutadd.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @RestController
 @RequestMapping(value="/api/user")
 public class UserController {
-	
+
 	@Autowired
 	private UserRepository repository;
 	@Autowired
 	private SessionIDRepository srepository;
-	
-	
+
+
 	@RequestMapping(value="/post/registration", method=RequestMethod.POST)
 	@ResponseBody
-	public void registration(HttpSession session,@RequestParam("name") String name,@RequestParam("password") String password,@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)@RequestParam("birth") LocalDate birth,@RequestParam("email") String email) {
+	public ResponseEntity<String> registration(HttpSession session,@RequestParam("name") String name,@RequestParam("UserID") String uID,@RequestParam("password") String password,@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)@RequestParam("birth") LocalDate birth,@RequestParam("email") String email) {
 		String sessionID=session.getId();
-		Random rn = new Random();
-		BigInteger uID= new BigInteger(255,rn);
 		User u=new User();
-		u.setUserID(uID.toString(16));
-		u.setName(name);
-		u.setPassword(password);
-		u.setEmail(email);
-		u.setBirth(birth.toString());
-		repository.save(u);
-		SessionID se=new SessionID();
-		se.setUserID(uID.toString(16));
-		se.setSessionID(sessionID);
-		srepository.save(se);
+		if(password.length()>=8) {
+			if(uID.matches("@[a-z]*[A-Z]*")) {
+				u.setUserID(uID);
+				u.setName(name);
+				u.setPassword(new BCryptPasswordEncoder().encode(password));
+				u.setEmail(email);
+				u.setBirth(birth.toString());
+				repository.save(u);
+				SessionID se=new SessionID();
+				se.setUserID(uID);
+				se.setSessionID(sessionID);
+				srepository.save(se);
+				return new ResponseEntity<>(HttpStatus.OK);
+			}else {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid ID");
+			}
+		}else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Password length liquire 8 or longer.");
+		}
 	}
 }
